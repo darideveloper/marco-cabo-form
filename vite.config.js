71bde734a26248f5927106661d3b8e047fb07f8a
daridev <darideveloper@gmail.com>
2025-08-16 19:29:45 -0600

    fix (vite): setup deploy from nyx

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/',
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    // react router fix 404
+    historyApiFallback: true,
+  },
 });
